3ac49a975fd51fb59f17ff15a82dc2a66523ccff
Shaiksafeer <shaiksafeer031@gmail.com>
2026-05-11 20:39:03 +0530

    exceptions changes

--- backend/src/main/java/com/teammanager/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/teammanager/exception/GlobalExceptionHandler.java
@@ -6,6 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -16,5 +19,18 @@ public class GlobalExceptionHandler {
                 .message(ex.getMessage())
                 .build();
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleException(Exception ex) {
+
+        ex.printStackTrace();
+
+        Map<String, Object> error = new HashMap<>();
+        error.put("message", ex.getMessage());
+        error.put("exception", ex.getClass().getName());
+
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(error);
     }
 }
